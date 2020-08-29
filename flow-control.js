eb3f1d435dc47482e0148c513489a3da82624e56
Alan Bielik <abielik92@gmail.com>
2020-08-29 20:49:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,6 +18,7 @@ return (age >=13 && age <=19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+  switch(age) {
   case 13:
   return "You are a teenager";
   break;
@@ -40,6 +41,6 @@ function switchAge(age) {
   return "You are a teenager";
   break;
   default:
-  return "You have an age";  
+  return "You have an age";  }
 
 }
