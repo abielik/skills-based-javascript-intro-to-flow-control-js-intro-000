44d8c511a4ad989af19d053a1bea48fbbd5000eb
Alan Bielik <abielik92@gmail.com>
2020-08-29 19:02:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,13 +3,13 @@ if(age>=13 && age<=19) { return = ("You are a teenager!");}
 }
 
 function teenager(age) {
-if (age >=13 && <=19) {"Your are a teenager!"
+if (age >=13 && age<=19) {"Your are a teenager!"
 } else {
   "You are not a teenager"}
 }
 
 function ageChecker(age) {
-if (age >=13 && <=19) {"You are a teenager!"
+if (age >=13 && age<=19) {"You are a teenager!"
 } else if (age <=12) {"You are a kid"
 } else (age >19) {"You are a grownup"}
 }
